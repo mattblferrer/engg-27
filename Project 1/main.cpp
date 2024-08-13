e8fe0f68b7dd3706efe13236c4dcd7a49fbe5ceb
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-13 16:48:07 +0800

    placeholder code

---
+++ Project 1/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * main.cpp
+ * 
+ * BERNARDO, Jonathan
+ * FERRER, Matt
+ * 
+ * ENGG 27 - M
+*/
+
+#include <iostream> 
+
+using namespace std;
+
+double maximum()
+{
+  return 0.0;
+}
+
+double minimum()
+{
+  return 0.0;
+}
+
+int main()
+{
+  cout << maximum() << endl;
+  cout << minimum() << endl;
+
+  return 0;
+}
